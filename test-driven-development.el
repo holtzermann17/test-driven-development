b946495e55c062e59af0fd162e2dd8e94b76ec0e
Joe Corneli <holtzermann17@gmail.com>
2013-11-13 22:31:41 +0000

    Add a little bit of documentation

--- test-driven-development.el
+++ test-driven-development.el
@@ -2,11 +2,13 @@
 
 ;;; Documentation: General Overview:
 
-;; The idea in this file is to use some variant of test driven development
-;; (for now, within the framework describe in ~/emacs-24.3/info/ert.info)
-;; as a way to keep track of an evolving model of a codebase.  It is build around
-;; a set of tests that instantiate the FACE model of computational creativity.
-;; This model is specified, at the outline level, as follows:
+;; The idea in this file is to use some variant of test
+;; driven development (for now, within the framework
+;; describe in ~/emacs-24.3/info/ert.info) as a way to
+;; keep track of an evolving model of a codebase.  It is
+;; build around a set of tests that instantiate the FACE
+;; model of computational creativity.  This model is
+;; specified, at the outline level, as follows:
 ;;
 ;;    F = framing
 ;;    A = aesthetic
@@ -15,125 +17,159 @@
 ;;
 ;; Potential uses of this system:
 ;; 
-;; - In a given instantiation of this programme, we might model something like the
-;; growth of the APM XI corpus through the addition of new examples and new
+;; - In a given instantiation of this programme, we might
+;; model something like the growth of the APM XI corpus
+;; through the addition of new examples and new
 ;; relationships between elements.
 ;;
-;; - In general, like with other kinds of test-driven development, we might write
-;; the tests "first" and use these to specify objectives as a "development
-;; roadmap" i.e. when those tests are passing, a given phase of the project is
-;; complete.
+;; - In general, like with other kinds of test-driven
+;; development, we might write the tests "first" and use
+;; these to specify objectives as a "development roadmap"
+;; i.e. when those tests are passing, a given phase of the
+;; project is complete.
 ;;
-;; Note that it seems vastly easier to build a particular instantiation of the
-;; FACE model than it is to build a general-purpose test that would always say
-;; "you have added a new concept" or "you have added a new aesthetic-method".
+;; Note that it seems vastly easier to build a particular
+;; instantiation of the FACE model than it is to build a
+;; general-purpose test that would always say "you have
+;; added a new concept" or "you have added a new
+;; aesthetic-method".
 ;;
-;; Instead of having a fully general model that works every time, we could instead
-;; come up with a type-theoretic treatment of the FACE schema, so that we could
-;; check whether a given system implements a test for a given facet of FACE.
+;; Instead of having a fully general model that works
+;; every time, we could instead come up with a
+;; type-theoretic treatment of the FACE schema, so that we
+;; could check whether a given system implements a test
+;; for a given facet of FACE.
 ;;
-;; If we go that route, it might make more sense to implement the test system in
-;; Haskell rather than LISP... but of course we actually want to be flexible, so
-;; we need to maintain a general sense of a test "implementing" a given type of
-;; test, across different programming languages - eventually, what we could
-;; provide in the COINVENT project is a set of sample implementations in various
-;; languages, together with an API for reporting/logging progress.  In order to
-;; make this attractive to potential users (and generate some empirical data) we
-;; might want to have a computational creativity contest.
+;; If we go that route, it might make more sense to
+;; implement the test system in Haskell rather than
+;; LISP... but of course we actually want to be flexible,
+;; so we need to maintain a general sense of a test
+;; "implementing" a given type of test, across different
+;; programming languages - eventually, what we could
+;; provide in the COINVENT project is a set of sample
+;; implementations in various languages, together with an
+;; API for reporting/logging progress.  In order to make
+;; this attractive to potential users (and generate some
+;; empirical data) we might want to have a computational
+;; creativity contest.
 ;;
-;; From a philosophical point of view, note that simply creating more and more
-;; concepts probably isn't the most "creative" -- actually, defining (and passing)
-;; new tests which get at different facets of creativity would tend to improve the
-;; overall "score".  Maybe each new test should be treated as a "multiplier" or
-;; something.
+;; From a philosophical point of view, note that simply
+;; creating more and more concepts probably isn't the most
+;; "creative" -- actually, defining (and passing) new
+;; tests which get at different facets of creativity would
+;; tend to improve the overall "score".  Maybe each new
+;; test should be treated as a "multiplier" or something.
 ;;
-;; From a procedural point of view, the system should, accordingly, work on
-;; several levels:
+;; From a procedural point of view, the system should,
+;; accordingly, work on several levels:
 ;;
-;; - A test that can detect when e.g. a new framing has been produced by
-;; looking for new documentation strings or whatever else qualifies as a "framing".
+;; - A test that can detect when e.g. a new framing has
+;; been produced by looking for new documentation strings
+;; or whatever else qualifies as a "framing".
 ;;
-;; - A test that can detect when a new *test* has been written, E.g. in addition
-;; to documentation strings, suppose we define a new kind of framing object called
-;; a "walkthrough", and we will consider a new framing to have been produced
-;; whenever either a new doc string or a new walkthrough is produced.  In the
-;; first place, we need to note the existence of the walkthrough test.
+;; - A test that can detect when a new *test* has been
+;; written, E.g. in addition to documentation strings,
+;; suppose we define a new kind of framing object called a
+;; "walkthrough", and we will consider a new framing to
+;; have been produced whenever either a new doc string or
+;; a new walkthrough is produced.  In the first place, we
+;; need to note the existence of the walkthrough test.
 ;;
-;; "Tests" themselves could be considered to be concepts or perhaps aesthetics
-;; (typically they map into {0,1}, but in general they could map to a set of error
-;; codes).
+;; "Tests" themselves could be considered to be concepts
+;; or perhaps aesthetics (typically they map into {0,1},
+;; but in general they could map to a set of error codes).
 ;;
-;; Note also that the tests generally need to be applied to the "increment", for
-;; instance, all *new* code that has been written since the last commit.  So, we
-;; need a method for extracting this increment.  I did something similar in my
-;; thesis that analysed git logs and looked for named entities in the diffs.
-;; Extracting a meaningful piece of code *directly* from a line-diff is not
-;; always going to be possible, since adding a single line typically isn't a
-;; "semantically" meaningful change.
+;; Note also that the tests generally need to be applied
+;; to the "increment", for instance, all *new* code that
+;; has been written since the last commit.  So, we need a
+;; method for extracting this increment.  I did something
+;; similar in my thesis that analysed git logs and looked
+;; for named entities in the diffs.  Extracting a
+;; meaningful piece of code *directly* from a line-diff is
+;; not always going to be possible, since adding a single
+;; line typically isn't a "semantically" meaningful
+;; change.
 ;;
-;; Let's assume that we can get ahold of the semantically meaningful changes with
-;; a little bit of code that scans a git commit and then reads the corresponding
-;; file.  Then we need to analyse this new code with the available tests -- and
-;; maybe, for user-friendly record-keeping, update the commit message
-;; automatically with a tally.  All of this git stuff is a fairly specific
-;; implementation detail.
+;; Let's assume that we can get ahold of the semantically
+;; meaningful changes with a little bit of code that scans
+;; a git commit and then reads the corresponding file.
+;; Then we need to analyse this new code with the
+;; available tests -- and maybe, for user-friendly
+;; record-keeping, update the commit message automatically
+;; with a tally.  All of this git stuff is a fairly
+;; specific implementation detail.
 ;;
-;; We could start with something simpler, like identifying the number of `defun'
-;; and `defvar' usages in a given file.  It's not entirely clear that the
-;; `ert-deftest' mechanism will be the best way to go, rather than using some
-;; other ad hoc code scanning and reporting mechanism, but we can try!
+;; We could start with something simpler, like identifying
+;; the number of `defun' and `defvar' usages in a given
+;; file.  It's not entirely clear that the `ert-deftest'
+;; mechanism will be the best way to go, rather than using
+;; some other ad hoc code scanning and reporting
+;; mechanism, but we can try!
 ;;
-;;  [After a first pass, I can see that ERT does work, but the reporting mechanism
-;;  isn't quite aligned with what we want to do, so after a prototyping phase, it
-;;  would be nice to augment ERT with some more "qualitative" reporting
-;;  mechanisms.]
+;;  [After a first pass, I can see that ERT does work, but
+;;  the reporting mechanism isn't quite aligned with what
+;;  we want to do, so after a prototyping phase, it would
+;;  be nice to augment ERT with some more "qualitative"
+;;  reporting mechanisms.]
 ;;
-;; The ERT documentation says "ERT only provides explanations for predicates that
-;; have an explanation function registered."  But for now, explanations are only
-;; triggered when a test fails, so the intuitive logic in the prototype is
-;; "reversed", i.e. "failing" is good, because it produces explanations.
+;; The ERT documentation says "ERT only provides
+;; explanations for predicates that have an explanation
+;; function registered."  But for now, explanations are
+;; only triggered when a test fails, so the intuitive
+;; logic in the prototype is "reversed", i.e. "failing" is
+;; good, because it produces explanations.
 ;;
 ;; They also say:
 ;;
-;;    ERT does not have built-in support for mocks or stubs.  The package
-;; `el-mock' (see `http://www.emacswiki.org/emacs/el-mock.el') offers
-;; mocks for Emacs Lisp and can be used in conjunction with ERT.
+;;    ERT does not have built-in support for mocks or
+;; stubs.  The package `el-mock' (see
+;; `http://www.emacswiki.org/emacs/el-mock.el') offers
+;; mocks for Emacs Lisp and can be used in conjunction
+;; with ERT.
 ;;
-;; Potentially worth checking this out when we get to the point of implementing a
-;; proper type theoretic schema.
+;; Potentially worth checking this out when we get to the
+;; point of implementing a proper type theoretic schema.
 
 ;;; Documentation: Implementation plan:
 
-;; The code in the ert-x package includes a macro for running tests on the current
-;; buffer, called `ert-with-buffer-renamed'.  In theory, we could use this to test
-;; how many matches for a given regexp are present in the buffer, say.  The one
-;; issue here is figuring out how to get ERT to report something qualitative,
-;; other than just pass/fail.
+;; The code in the ert-x package includes a macro for
+;; running tests on the current buffer, called
+;; `ert-with-buffer-renamed'.  In theory, we could use
+;; this to test how many matches for a given regexp are
+;; present in the buffer, say.  The one issue here is
+;; figuring out how to get ERT to report something
+;; qualitative, other than just pass/fail.
 ;;
-;; We have a sort of cheap method of doing that now, in the section "Implement a
-;; cheap counting test".
+;; We have a sort of cheap method of doing that now, in
+;; the section "Implement a cheap counting test".
 ;;
-;; We can have the code take in an "increment" to scan, see "Pass in an increment"
+;; We can have the code take in an "increment" to scan,
+;; see "Pass in an increment"
 ;;
-;; We can run all of the tests on a selected increment, see "Run a battery of
-;; tests on a given file".
+;; We can run all of the tests on a selected increment,
+;; see "Run a battery of tests on a given file".
 ;;
-;; Next up, we would want to do a bit of "model selection", i.e. check which of
-;; the 8 possible tests are actually implemented, and run only those.  Although
-;; actually there are 8 *types* of test, and not just 8 tests.  Maybe if I take
-;; out the "shoulds" the tests will automatically pass?  Yes, that's true.  So,
-;; this way I will only see reporting on the tests that are actually implemented.
+;; Next up, we would want to do a bit of "model
+;; selection", i.e. check which of the 8 possible tests
+;; are actually implemented, and run only those.  Although
+;; actually there are 8 *types* of test, and not just 8
+;; tests.  Maybe if I take out the "shoulds" the tests
+;; will automatically pass?  Yes, that's true.  So, this
+;; way I will only see reporting on the tests that are
+;; actually implemented.
 ;;
-;; It seems like I might as well just go ahead and implement this in ONE case
-;; (e.g. the APMXI case) and then I can take another pass to implement it in a
-;; more general type theoretic way.
+;; It seems like I might as well just go ahead and
+;; implement this in ONE case (e.g. the APMXI case) and
+;; then I can take another pass to implement it in a more
+;; general type theoretic way.
 ;;
 ;; [...]
 ;;
-;; Develop a nicer reporting framework so that we're not just seeing a bunch of
-;; failed tests, but instead, a nice-looking summary.  Note that this is not
-;; separate from the type theoretic issues - we want to have a "score card"
-;; for each increment.
+;; Develop a nicer reporting framework so that we're not
+;; just seeing a bunch of failed tests, but instead, a
+;; nice-looking summary.  Note that this is not separate
+;; from the type theoretic issues - we want to have a
+;; "score card" for each increment.
 
 ;;; Code:
 
@@ -144,9 +180,10 @@
 
 ;;; Basic versions
 
-;; These tests should take an "increment" as an argument.  We'll have to come up
-;; with a way to pass in a buffer or string to the test, perhaps using a fixture
-;; (see below).
+;; These tests should take an "increment" as an argument.
+;; We'll have to come up with a way to pass in a buffer or
+;; string to the test, perhaps using a fixture (see
+;; below).
 
 (ert-deftest FACE-new-framing-produced ()
   "Test whether a new framing exists now (for some definition of \"framing\").
@@ -166,6 +203,7 @@ Example: this concept is interesting, but it needs:
 - a relationship to another concept in the domain
 - a conjecture
 etc."
+
   ;; jac - Note that even more complex version would allow a map to another manifold.
   ;(should (= (+ 1 2) 4))
   )
@@ -235,8 +273,9 @@ of a method for doing other proofs."
 
 ;;; Implement a cheap counting test:
 
-;; The test "fails", but produces the actual number of "ert-deftests" as part of
-;; the explanation, which is what I was going for.  I can define similar counting
+;; The test "fails", but produces the actual number of
+;; "ert-deftests" as part of the explanation, which is
+;; what I was going for.  I can define similar counting
 ;; tests for the number of defuns and number of defvars
 
 (ert-deftest FACE-count-tests ()
@@ -256,11 +295,13 @@ of a method for doing other proofs."
 
 ;;; Pass in an increment:
 
-;; This is trickier, because the increment has to be sent as an argument.  I guess
-;; I can create a fixture that allows the user to interactively supply the name of
-;; a buffer to test, for example.
+;; This is trickier, because the increment has to be sent
+;; as an argument.  I guess I can create a fixture that
+;; allows the user to interactively supply the name of a
+;; buffer to test, for example.
 ;;
-;; OK, this code all works, but I'm commenting out for now b/c the interaction would be annoying when I'm testing
+;; OK, this code all works, but I'm commenting out for now
+;; b/c the interaction would be annoying when I'm testing
 
 ;(defun FACE-select-buffer-to-count-fixture (body)
 ;  (let ((buffer (read-buffer "Buffer to scan: ")))
@@ -278,10 +319,12 @@ of a method for doing other proofs."
 
 ;;; Run a battery of tests on a given file:
 
-;; All I really need is a function similar to `ert-run-tests-interactively', but
-;; that takes an argument, and makes that available to the various tests before
-;; the function runs.  That's easy.  Later we'll want a version that knows where
-;; to look for the increment, and just produces a report.
+;; All I really need is a function similar to
+;; `ert-run-tests-interactively', but that takes an
+;; argument, and makes that available to the various tests
+;; before the function runs.  That's easy.  Later we'll
+;; want a version that knows where to look for the
+;; increment, and just produces a report.
 
 (defvar FACE-buffer-as-increment nil)
 
